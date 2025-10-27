df05c5887beb0b6db2eeb349d6ec108da4c67322
manpreet-stellen <manpreet@stelleninfotech.in>
2025-10-27 23:48:11 +0530

    ci/cd pipeline successful and github action run failed fixed

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,11 @@
 <?php
 
 test('the application returns a successful response', function () {
+    // Ensure we have an encryption key
+    if (empty(config('app.key'))) {
+        $this->artisan('key:generate');
+    }
+    
     $response = $this->get('/');
-
     $response->assertStatus(200);
 });
